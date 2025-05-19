a9571ddaaabbeba5a43d69cd6a8c2ad63e67f935
Joshua Davies <cinchtail@gmail.com>
2025-05-18 22:59:23 -0400

    loot table stuff

--- src/main/java/net/cinchtail/fullyfarmablesand/FullyFarmableSand.java
+++ src/main/java/net/cinchtail/fullyfarmablesand/FullyFarmableSand.java
@@ -1,5 +1,6 @@
 package net.cinchtail.fullyfarmablesand;
 
+import net.cinchtail.fullyfarmablesand.loot.ModLootTableModifiers;
 import net.fabricmc.api.ModInitializer;
 
 import org.slf4j.Logger;
@@ -11,5 +12,6 @@ public class FullyFarmableSand implements ModInitializer {
 
 	@Override
 	public void onInitialize() {
+		ModLootTableModifiers.modifyLootTables();
 	}
 }
