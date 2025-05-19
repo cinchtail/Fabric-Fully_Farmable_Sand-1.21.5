b0590bcb247977dfe747c33a65040440c03a46f9
Joshua Davies <cinchtail@gmail.com>
2025-05-19 00:48:31 -0400

    loot table and structure started

--- src/main/java/net/cinchtail/fullyfarmablesand/loot/ModLootTableModifiers.java
+++ src/main/java/net/cinchtail/fullyfarmablesand/loot/ModLootTableModifiers.java
@@ -11,18 +11,16 @@ import net.minecraft.loot.provider.number.ConstantLootNumberProvider;
 import net.minecraft.loot.provider.number.UniformLootNumberProvider;
 import net.minecraft.util.Identifier;
 
-
 public class ModLootTableModifiers {
-    private static final Identifier HUSK_ID =
-            new Identifier("minecraft", "entities/husk");
+    private static final Identifier HUSK_ID
+            = Identifier.of("minecraft", "entities/husk");
 
     public static void modifyLootTables() {
-        LootTableEvents.MODIFY.register((resourceManager, lootManager, id, tableBuilder, source) -> {
-
-            if(HUSK_ID.equals(id)) {
+        LootTableEvents.MODIFY.register((key, tableBuilder, source, registry) -> {
+            if(HUSK_ID.equals(key.getValue())) {
                 LootPool.Builder poolBuilder = LootPool.builder()
                         .rolls(ConstantLootNumberProvider.create(1))
-                        .conditionally(RandomChanceLootCondition.builder(.8f))
+                        .conditionally(RandomChanceLootCondition.builder(1f))
                         .with(ItemEntry.builder(Items.SAND))
                         .apply(SetCountLootFunction.builder(UniformLootNumberProvider.create(1.0f, 4.0f)).build());
 
