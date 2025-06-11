715886fe07c14830125ca0318558ab617597284f
Joshua Davies <cinchtail@gmail.com>
2025-06-10 21:15:53 -0400

    All added

---
+++ src/main/java/net/cinchtail/fullyfarmablesand/FullyFarmableSand.java
@@ -0,0 +1,17 @@
+package net.cinchtail.fullyfarmablesand;
+
+import net.cinchtail.fullyfarmablesand.loot.ModLootTableModifiers;
+import net.fabricmc.api.ModInitializer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class FullyFarmableSand implements ModInitializer {
+	public static final String MOD_ID = "fullyfarmablesand";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+		ModLootTableModifiers.modifyLootTables();
+	}
+}
---
+++ src/main/java/net/cinchtail/fullyfarmablesand/loot/ModLootTableModifiers.java
@@ -0,0 +1,30 @@
+package net.cinchtail.fullyfarmablesand.loot;
+
+import net.fabricmc.fabric.api.loot.v3.LootTableEvents;
+import net.minecraft.entity.EntityType;
+import net.minecraft.item.Items;
+import net.minecraft.loot.LootPool;
+import net.minecraft.loot.condition.RandomChanceLootCondition;
+import net.minecraft.loot.entry.ItemEntry;
+import net.minecraft.loot.function.EnchantedCountIncreaseLootFunction;
+import net.minecraft.loot.function.SetCountLootFunction;
+import net.minecraft.loot.provider.number.ConstantLootNumberProvider;
+import net.minecraft.loot.provider.number.UniformLootNumberProvider;
+
+public class ModLootTableModifiers {
+
+    public static void modifyLootTables() {
+        LootTableEvents.MODIFY.register((key, tableBuilder, source, registry) -> {
+            if (key.equals(EntityType.HUSK.getLootTableId())) {
+                LootPool.Builder poolBuilder = LootPool.builder()
+                        .rolls(ConstantLootNumberProvider.create(1))
+                        .conditionally(RandomChanceLootCondition.builder(1f))
+                        .with(ItemEntry.builder(Items.SAND)
+                                .apply(SetCountLootFunction.builder(UniformLootNumberProvider.create(1.0f,4.0f)))
+                                .apply(EnchantedCountIncreaseLootFunction.builder(registry, UniformLootNumberProvider.create(.5f, 1))));
+
+                tableBuilder.pool(poolBuilder.build());
+            }
+        });
+    }
+}
